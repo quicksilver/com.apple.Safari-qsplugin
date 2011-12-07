ba078bddc8a795f123f02653a5d93324872b5d9d
Rob McBroom <github@skurfer.com>
2011-12-07 11:33:32 -0500

    create a new class for Safari-specific actions

---
+++ QSSafariActionProvider.h
@@ -0,0 +1,12 @@
+//
+//  QSSafariActionProvider.h
+//  QSSafariPlugIn
+//
+//  Created by Rob McBroom on 2011/12/7.
+//
+
+#import <QSCore/QSCore.h>
+
+@interface QSSafariActionProvider : QSActionProvider
+
+@end
---
+++ QSSafariActionProvider.m
@@ -0,0 +1,12 @@
+//
+//  QSSafariActionProvider.m
+//  QSSafariPlugIn
+//
+//  Created by Rob McBroom on 2011/12/7.
+//
+
+#import "QSSafariActionProvider.h"
+
+@implementation QSSafariActionProvider
+
+@end
