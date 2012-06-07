931f446d99b034d6980b683d58cc257d773856b9
Rob McBroom <github@skurfer.com>
2012-06-07 14:25:29 -0400

    switch to fast enumeration

--- QSSafariPlugin.m
+++ QSSafariPlugin.m
@@ -230,10 +230,7 @@
 	NSString *title = [dict objectForKey:@"Title"];
 	if ([title isEqualToString:@"Archive"]) return nil; //***Skip Archive Folder
 	
-	
-	NSEnumerator *childEnum = [[dict objectForKey:@"Children"] objectEnumerator];
-	NSDictionary *child;
-	while (child = [childEnum nextObject]) {
+	for (NSDictionary *child in [dict objectForKey:@"Children"]) {
 		NSString *type = [child objectForKey:@"WebBookmarkType"];
 		QSObject *object = nil;
 		if ([type isEqualToString:@"WebBookmarkTypeLeaf"]) {
@@ -316,21 +313,15 @@
 
 - (NSArray *)objectsFromPath:(NSString *)path withSettings:(NSDictionary *)settings {
     NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: [path stringByStandardizingPath]];
-    NSArray *history = [dict objectForKey:@"WebHistoryDates"];
-    
-    
     NSMutableArray *array = [NSMutableArray arrayWithCapacity:1];
     
-    NSEnumerator *childEnum = [history objectEnumerator];
-    NSDictionary *child;
-    while (child = [childEnum nextObject]) {
+    for (NSDictionary *child in [dict objectForKey:@"WebHistoryDates"]) {
         NSString *url = [child objectForKey:@""];
         NSString *title = [child objectForKey:@"title"];
         QSObject *object = [QSObject URLObjectWithURL:url title:title];
         if (object) [array addObject:object];
     }
     return  array;
-    
 }
 
 @end
